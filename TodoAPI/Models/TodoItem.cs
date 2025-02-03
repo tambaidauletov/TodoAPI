384f27f0a58aad530e1d235bd274a20fdfbf275b
unknown <tbaidaul@my.centennialcollege.ca>
2025-02-03 12:55:24 -0500

    Enhancement #<1>: Added Quiz #2 item to TodoItems

--- TodoAPI/Models/TodoItem.cs
+++ TodoAPI/Models/TodoItem.cs
@@ -6,4 +6,14 @@
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
     }
+
+    public static class TodoData
+    {
+        public static List<TodoItem> Items = new List<TodoItem>
+        {
+            new TodoItem { Id = 1, Name = "Quiz #1", IsComplete = true },
+            new TodoItem { Id = 2, Name = "Assignment #1", IsComplete = false },
+            new TodoItem { Id = 3, Name = "Quiz #2", IsComplete = false }
+        };
+    }
 }
